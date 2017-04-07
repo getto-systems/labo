49a2f7309ae6446823e9e2abf8a3c24ad69ccfa2
shun <shun@getto.systems>
2017-04-07 19:07:58 +0900

    feature reconfigure ssh server keys

--- docker-entrypoint.sh
+++ docker-entrypoint.sh
@@ -1,7 +1,9 @@
 #!/bin/bash
 
 if [ "$1" = "/usr/sbin/sshd" ]; then
-	mkdir -p /var/run/sshd
+  mkdir -p /var/run/sshd
+  rm -rf /etc/ssh/ssh_host_*
+  dpkg-reconfigure openssh-server
 fi
 
 env | grep "^DOCKER_" > /etc/docker-env
